7acab51be14d6a568d3469271683f732b74a3179
Tien Butts <tienbutts@gmail.com>
2015-12-01 16:38:48 -0500

    Addition of Assets Folder

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 var map = L.map('map').setView([40.683505, -73.948776], 13); 
 
+var circleIcon = L.icon ({
+    iconUrl:'/assets/circle-pointer.png',
+
+    iconSize:     [40, 40],
+    iconAnchor:   [22, 94],
+})
+
+
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6IjZjNmRjNzk3ZmE2MTcwOTEwMGY0MzU3YjUzOWFmNWZhIn0.Y8bhBaUMqFiPrDRW9hieoQ', { 
         maxZoom: 18, 
         id: 'mapbox.streets' 
     }).addTo(map); 
 
-L.marker([40.683505, -73.948776]).addTo(map) 
+L.marker([40.683505, -73.948776], {icon: circleIcon}).addTo(map) 
 .bindPopup("<b>Home</b>").openPopup(); 
 
 L.marker([40.716484, -73.988723]).addTo(map) 
@@ -15,15 +23,31 @@ L.marker([40.726430, -73.984101]).addTo(map)
 .bindPopup("<b>Big Gay Ice Cream</b>").openPopup(); 
 
 L.marker([40.722514, -73.985741]).addTo(map) 
-.bindPopup("<b>Big Gay IceCream</b>").openPopup(); 
+.bindPopup("<b>Double Down Saloon</b>").openPopup(); 
 
-var popup = L.popup(); 
+L.marker([40.795429, -73.930289]).addTo(map) 
+.bindPopup("<b>Costco: Harlem</b>").openPopup();
 
-function onMapClick(e) { 
-    popup
-        .setLatLng(e.latlng) 
-        .setContent("You clicked the map at " + e.latlng.toString()) 
-        .openOn(map); 
-    }
+L.marker([40.767276, -73.937568]).addTo(map) 
+.bindPopup("<b>Costco: Astoria</b>").openPopup(); 
 
-map.on('click', onMapClick); 
+L.marker([40.655620, -74.009233]).addTo(map) 
+.bindPopup("<b>Costco: Brooklyn</b>").openPopup(); 
+
+L.marker([40.645556,-74.011944]).addTo(map) 
+.bindPopup("<b>Sunset Park</b>").openPopup();  
+
+L.marker([40.765830, -73.833084]).addTo(map) 
+.bindPopup("<b>Flushing</b>").openPopup(); 
+
+L.marker([40.714722,-73.997222]).addTo(map) 
+.bindPopup("<b>Chinatown</b>").openPopup(); 
+
+L.marker([40.805187, -73.966378]).addTo(map) 
+.bindPopup("<b>The Heights</b>").openPopup(); 
+
+L.marker([40.723833, -74.004279]).addTo(map) 
+.bindPopup("<b>Black Tap</b>").openPopup(); 
+
+L.marker([40.714176, -73.986960]).addTo(map) 
+.bindPopup("<b>Ice & Vice</b>").openPopup(); 
